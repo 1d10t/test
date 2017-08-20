0b3ef69d1bbe133cd3548386f86441fc11adcc9d
Sergey S Yaglov <sergey@yaglov.ru>
2017-08-21 01:22:52 +0300

    remove noice

--- geo-watch.js
+++ geo-watch.js
@@ -1,22 +1,22 @@
 function geo_watch(success, error) {
-  
-  if(!error)
-    error = function(err) { console.warn(`ERROR(${err.code}): ${err.message}`); };
-  
-  if (!("geolocation" in navigator)){
-    error({code: 0, message: 'Geolocation is not supported by your browser'});
-    return;
-  }
-  
-  return navigator.geolocation.watchPosition(success, error, {enableHighAccuracy: true, maximumAge: 0, timeout: 27000});
+	
+	if(!error)
+		error = function(err) { console.warn(`ERROR(${err.code}): ${err.message}`); };
+	
+	if (!("geolocation" in navigator)){
+		error({code: 0, message: 'Geolocation is not supported by your browser'});
+		return;
+	}
+	
+	return navigator.geolocation.watchPosition(success, error, {enableHighAccuracy: true, maximumAge: 0, timeout: 27000});
 }
 
 var geo_watch_id = geo_watch(function(position){
-  var crd = position.coords;
-  console.log('Your current position is:');
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
+	var crd = position.coords;
+	console.log('Your current position is:');
+	console.log(`Latitude : ${crd.latitude}`);
+	console.log(`Longitude: ${crd.longitude}`);
+	console.log(`More or less ${crd.accuracy} meters.`);
 });
 
 //navigator.geolocation.clearWatch(geo_watch_id);
